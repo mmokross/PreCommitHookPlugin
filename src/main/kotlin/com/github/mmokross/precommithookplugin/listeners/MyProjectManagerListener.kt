bd3697e0135ec3cc6c1ad397a652d2fc30959106
GitHub Action <action@github.com>
2022-03-24 10:55:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/mmokross/precommithookplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/mmokross/precommithookplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mmokross.precommithookplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mmokross.precommithookplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/mmokross/precommithookplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mmokross.precommithookplugin.services
+
+import com.github.mmokross.precommithookplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
